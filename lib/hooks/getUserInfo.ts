3f03f3ea5f4d5a06ea13614a16274ff76616e1c6
richard06 <richardmacabus06@gmail.com>
2026-01-23 01:38:17 +0800

    configured lib and hooks javascript files to typescript

--- lib/hooks/getUserInfo.ts
+++ lib/hooks/getUserInfo.ts
@@ -2,7 +2,7 @@ import { authClient } from "@/lib/auth-client" // import the auth client
 
 export function User(){
   const { 
-      session, 
+      data: session, 
       isPending, //loading state
       error, //error object
       refetch //refetch the session
