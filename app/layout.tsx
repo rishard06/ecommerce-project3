03bc81b37274efe34f59789fd8aa0408e94afd47
richard06 <richardmacabus06@gmail.com>
2026-02-17 21:50:28 +0800

    add primary colors on globals.css remove inter in layout.tsx set prisma to version 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
